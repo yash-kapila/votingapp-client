bf9fb8fdad44bc14cf49ad4ccb0201c5d22873ae
Yash Kapila <yash.kapila18@gmail.com>
2016-10-27 13:04:22 +0000

    app commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 
+// serve static files
+app.use(express.static('./_build/app'));
+
 // keep port number configuration in a config file for different environments
-app.listen(3000, function(){
-    console.log("Listening at port 3000");
+app.listen(8080, function(){
+    console.log("Listening at port 8080");
 })
 
 // expose app           
