7914c5616ba05260c5f428d674a884931f75c358
Yash Kapila <yash.kapila18@gmail.com>
2016-10-28 05:23:03 +0000

    server.js commit

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var app = express();
+var proxy = require('express-http-proxy');
 
 // serve static files
 app.use(express.static('./_build/app'));
+
+// proxy requests from client to server after rewriting the url and appending '/api' to it
+app.use('/api', proxy('https://votingapp-server-yashkapila.c9users.io', {
+    forwardPath: function(req, res) {
+        return require('url').parse('/api'+req.url).path;
+    }
+}));
 
 // keep port number configuration in a config file for different environments
 app.listen(8080, function(){
